b445317bdd7982d988783c51e0f431dd69b33ef4
jvjue <799525644@qq.com>
2021-03-12 16:31:24 +0800

    hellovue

---
+++ 01 hello vue/Hello.vue
@@ -0,0 +1,36 @@
+<!-- vbase快捷生成 -->
+<template>
+    <div>
+        <!-- vfor快捷生成 -->
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        // vdata快速生成
+        data() {
+            return {
+                key: value
+            }
+        },
+        // vmethod
+        methods: {
+            name() {
+                
+            }
+        },
+        // vcomputed
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
